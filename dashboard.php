3bebf7e0f9635fa378bb1c65aa685e226b6b5ad2
Shatrudhan <shatrudhan.kumar@123789.org>
2022-09-16 03:19:00 +0530

    dashboard

--- dashboard.php
+++ dashboard.php
@@ -7,25 +7,27 @@ if(!isset($_SESSION['userdata'])){
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <title>Dashboard</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
-</head>
-<body>
-
-<div class="container">
-<nav class="navbar navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand">Welcome, <?= $_SESSION['userdata']['name']; ?></a>
-    <h3 class="text-center">Dashbaord</h3> 
-    <form class="d-flex">
-      <a href="logout.php" class="btn btn-outline-danger" type="submit">Logout</a>
-    </form>
-  </div>
-</nav>
-</div>
-</body>
+    <head>
+        <title>Dashboard</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-light bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand">
+                        Welcome,
+                        <?= $_SESSION['userdata']['name']; ?>
+                    </a>
+                    <h3 class="text-center">Dashbaord</h3>
+                    <form class="d-flex">
+                        <a href="logout.php" class="btn btn-outline-danger" type="submit">Logout</a>
+                    </form>
+                </div>
+            </nav>
+        </div>
+    </body>
 </html>
